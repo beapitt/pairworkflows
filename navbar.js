22d52c8e4697d272ac2a4067a66f5a7874b8d872
beapitt <salvatibeatricebautista@gmail.com>
2026-06-30 15:40:15 +0200

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -242,6 +242,7 @@
 
         <!-- DESKTOP LINKS -->
         <div class="nav-desktop-links" style="display:flex;align-items:center;gap:4px;">
+        <a href="https://www.pairworkflows.com/index.html" class="nav-link">All Courses</a>
 
           <!-- PROMPTS DROPDOWN -->
           <div class="nav-dropdown">
